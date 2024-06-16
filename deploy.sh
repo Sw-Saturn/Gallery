c9fcc0b7c9ed492ec6187461986aa25f90f33908
Kanta Demizu <demizu.1203@gmail.com>
2024-06-16 16:24:12 +0900

    rebuilding site 2024年 6月16日 日曜日 16時24分12秒 JST

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo --cleanDestinationDir # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo --gc --cleanDestinationDir --minify # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 \cp -p -f -R public/* docs
 git add .
